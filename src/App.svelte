644ba381c741002abc4e4f899765b68349bd3cf2
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-02 17:25:58 -0400

    Initialize Svelte app

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let name;
+</script>
+
+<style>
+  h1 {
+    background-color: #424242;
+    color: white;
+    right: 5rem;
+  }
+  img { position: absolute; }
+  ul { list-style: none; }
+</style>
+
+<img src="/assets/test.png" alt=""/>
+<h1>Hello {name}!</h1>
+<ul>
+  <li>
+    <a href="projects.mrewers.com" target="_blank">Projects</a>
+  </li>
+  <li>
+    <a href="books.mrewers.com" target="_blank">Book Club</a>
+  </li>
+</ul>
