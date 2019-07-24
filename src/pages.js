19fca4e3f4a23e845b4db5f66f072bedb4927239
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-24 17:59:39 -0400

    Add initial data

---
+++ src/pages.js
@@ -0,0 +1,13 @@
+export const pages = [
+  {
+    github: "https://github.com/mrewers/Librum",
+    screenshot: "/assets/books.jpg",
+    title: "Book Club",
+    url: "https://books.mrewers.com"
+  },
+  {
+    screenshot: "/assets/projects.png",
+    title: "Projects",
+    url: "https://projects.mrewers.com"
+  }
+];
